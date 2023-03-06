8a7c85c1a8195ae3315b4263d193919fb6b9d7a7
António Quaresma <antqua@outlook.pt>
2023-03-06 19:59:10 +0000

    Set getPlayerSelection() function
    The player can input 'Rock' 'Paper' or 'Scissors'
    In case of wrong input the default choice will be Rock

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 // GLOBAL VARIABLES -----------------------------------------------------
 let gameIsRunning = false;
-
 let playerName;
 
-
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_PLAYER_CHOICE = ROCK;
 
 // FUNCTIONS & GAME LOGIC ----------------------------------------------
+const getPlayerSelection = () => {
+    const playerSelection = prompt(
+      `Choose ${ROCK}, ${PAPER} or ${SCISSORS}`,
+      ''
+    ).toUpperCase();
+    if (
+      playerSelection !== ROCK &&
+      playerSelection !== PAPER &&
+      playerSelection !== SCISSORS
+    ) {
+      alert(`Invalid choice! We choose ${DEFAULT_PLAYER_CHOICE} for you!`);
+      //return DEFAULT_PLAYER_CHOICE;
+      return;
+    }
+    return playerSelection;
+  };
 
 function startGame() {
     // ensure that once we started a game we cannot start another game
@@ -17,7 +35,12 @@ function startGame() {
     //console.log('Game is starting...');
     alert('Game is starting...');
   
+  //TODO:
+  //- insert function to ask the player name and save it
+  //- insert function to greet the player with is name
 
+  const playerSelection = getPlayerSelection();
+  console.log(playerSelection);
   
     gameIsRunning = false;
   }
